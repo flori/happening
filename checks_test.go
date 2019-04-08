6c2fd153a823021f142976297c81482f54d386f5
Florian Frank <flori@ping.de>
2019-04-08 14:48:02 +0200

    Test some check transitions

--- checks_test.go
+++ checks_test.go
@@ -1,16 +1,69 @@
 package happening
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
 )
 
 var testAPI = &API{
 	NOTIFIER: &NullNotifier{},
 }
 
-func TestComputeHealthStatus(t *testing.T) {
-	checks := new([]Check)
-	computeHealthStatus(testAPI, checks)
-	assert.True(t, true)
+func TestComputeHealthNoSuccess(t *testing.T) {
+	checks := []Check{
+		Check{
+			Healthy:    true,
+			Success:    false,
+			LastPingAt: time.Now(),
+			Period:     time.Second,
+		},
+	}
+	assert.True(t, checks[0].Healthy)
+	computeHealthStatus(testAPI, &checks)
+	assert.False(t, checks[0].Healthy)
+}
+
+func TestComputeHealthSuccess(t *testing.T) {
+	checks := []Check{
+		Check{
+			Healthy:    false,
+			Success:    true,
+			LastPingAt: time.Now(),
+			Period:     time.Second,
+		},
+	}
+	assert.False(t, checks[0].Healthy)
+	computeHealthStatus(testAPI, &checks)
+	assert.True(t, checks[0].Healthy)
+}
+
+func TestComputeHealthTimeout(t *testing.T) {
+	oldTime := time.Now().Add(-2 * time.Second)
+	checks := []Check{
+		Check{
+			Healthy:    true,
+			Success:    true,
+			LastPingAt: oldTime,
+			Period:     time.Second,
+		},
+	}
+	assert.True(t, checks[0].Healthy)
+	computeHealthStatus(testAPI, &checks)
+	assert.False(t, checks[0].Healthy)
+}
+
+func TestComputeHealthTimeoutResolved(t *testing.T) {
+	checks := []Check{
+		Check{
+			Healthy:    false,
+			Success:    true,
+			LastPingAt: time.Now(),
+			Period:     2 * time.Second,
+		},
+	}
+	assert.False(t, checks[0].Healthy)
+	computeHealthStatus(testAPI, &checks)
+	assert.True(t, checks[0].Healthy)
 }
