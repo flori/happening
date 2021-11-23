cad804cb8ba970db01a0b04f8148bdc7c8e7c410
Florian Frank <flori@ping.de>
2021-11-23 04:06:44 +0100

    Extract JWT functions

--- cmd/happening-server/main.go
+++ cmd/happening-server/main.go
@@ -4,27 +4,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"strings"
-	"time"
 
 	happening "github.com/flori/happening"
 	"github.com/go-playground/validator"
-	"github.com/golang-jwt/jwt"
 	"github.com/kelseyhightower/envconfig"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
-
-func jwtAuth(config happening.ServerConfig) middleware.JWTConfig {
-	return middleware.JWTConfig{
-		Skipper: func(c echo.Context) bool {
-			path := c.Path()
-			method := c.Request().Method
-			return path == "/api/v1/event" && (method == "POST" || method == "PUT")
-		},
-		SigningKey: []byte(config.SIGNING_SECRET),
-	}
-}
 
 type errorsResponse struct {
 	Success bool   `json:"success"`
@@ -59,55 +45,6 @@ func (cv *CustomValidator) Validate(i interface{}) error {
 	return cv.validator.Struct(i)
 }
 
-// jwtCustomClaims are custom claims extending default ones.
-// See https://github.com/golang-jwt/jwt for more examples
-type jwtCustomClaims struct {
-	Name string `json:"name"`
-	jwt.StandardClaims
-}
-
-type AuthPair struct {
-	Username string `json:"username" validate:"required"`
-	Password string `json:"password" validate:"required"`
-}
-
-func jwtLoginWithConfig(config happening.ServerConfig) func(echo.Context) error {
-	return func(c echo.Context) error {
-		authPair := new(AuthPair)
-		if err := c.Bind(authPair); err != nil {
-			return err
-		}
-
-		httpAuth := strings.Split(config.HTTP_AUTH, ":")
-
-		// Throws unauthorized error
-		if authPair.Username != httpAuth[0] || authPair.Password != httpAuth[1] {
-			return echo.ErrUnauthorized
-		}
-
-		// Set custom claims
-		claims := &jwtCustomClaims{
-			"Admin",
-			jwt.StandardClaims{
-				ExpiresAt: time.Now().Add(time.Hour * 24 * 7).Unix(), // 1 week
-			},
-		}
-
-		// Create token with claims
-		token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
-
-		// Generate encoded token and send it as response.
-		t, err := token.SignedString([]byte(config.SIGNING_SECRET))
-		if err != nil {
-			return err
-		}
-
-		return c.JSON(http.StatusOK, echo.Map{
-			"token": t,
-		})
-	}
-}
-
 func main() {
 	var config happening.ServerConfig
 	err := envconfig.Process("", &config)
@@ -135,9 +72,9 @@ func main() {
 		log.Fatal("Need HTTP_AUTH and SIGNING_SECRET configuration to start server.")
 	} else {
 		fmt.Println("info:dmi Configuring JWT authentication")
-		g.Use(middleware.JWTWithConfig(jwtAuth(config)))
+		g.Use(middleware.JWTWithConfig(happening.JwtAuth(config)))
 	}
-	e.POST("/jwt", jwtLoginWithConfig(config))
+	e.POST("/jwt", happening.JwtLoginWithConfig(config))
 	if config.NOTIFIER_KIND != "" {
 		log.Printf("Using notifier for %s", config.NOTIFIER_KIND)
 	} else {
---
+++ jwt.go
@@ -0,0 +1,71 @@
+package happening
+
+import (
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+func JwtAuth(config ServerConfig) middleware.JWTConfig {
+	return middleware.JWTConfig{
+		Skipper: func(c echo.Context) bool {
+			path := c.Path()
+			method := c.Request().Method
+			return path == "/api/v1/event" && (method == "POST" || method == "PUT")
+		},
+		SigningKey: []byte(config.SIGNING_SECRET),
+	}
+}
+
+// jwtCustomClaims are custom claims extending default ones.
+// See https://github.com/golang-jwt/jwt for more examples
+type jwtCustomClaims struct {
+	Name string `json:"name"`
+	jwt.StandardClaims
+}
+
+type AuthPair struct {
+	Username string `json:"username" validate:"required"`
+	Password string `json:"password" validate:"required"`
+}
+
+func JwtLoginWithConfig(config ServerConfig) func(echo.Context) error {
+	return func(c echo.Context) error {
+		authPair := new(AuthPair)
+		if err := c.Bind(authPair); err != nil {
+			return err
+		}
+
+		httpAuth := strings.Split(config.HTTP_AUTH, ":")
+
+		// Throws unauthorized error
+		if authPair.Username != httpAuth[0] || authPair.Password != httpAuth[1] {
+			return echo.ErrUnauthorized
+		}
+
+		// Set custom claims
+		claims := &jwtCustomClaims{
+			"Admin",
+			jwt.StandardClaims{
+				ExpiresAt: time.Now().Add(time.Hour * 24 * 7).Unix(), // 1 week
+			},
+		}
+
+		// Create token with claims
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+		// Generate encoded token and send it as response.
+		t, err := token.SignedString([]byte(config.SIGNING_SECRET))
+		if err != nil {
+			return err
+		}
+
+		return c.JSON(http.StatusOK, echo.Map{
+			"token": t,
+		})
+	}
+}
