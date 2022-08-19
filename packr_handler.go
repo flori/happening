da7d463024567f3a0e5bc7c5c3bd79f612074bb7
Florian Frank <flori@ping.de>
2022-08-19 02:34:23 +0200

    Log handler decision

--- packr_handler.go
+++ packr_handler.go
@@ -59,12 +59,14 @@ func PackrHandler(config ServerConfig) echo.HandlerFunc {
 			oldURL := c.Request().URL
 			path := oldURL.Path
 			if path != "" && box.Has(path) {
+				log.Printf("Serving %s from box.", path)
 				h.ServeHTTP(c.Response(), c.Request())
 				return nil
 			} else {
 				if path == "" {
 					path = "/search"
 				}
+				log.Printf("Redirecting URL from %s to %s.", oldURL, path)
 				return c.Redirect(http.StatusTemporaryRedirect, redirectURL(path, oldURL))
 			}
 		}
